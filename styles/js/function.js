0a22f22c7dcf5ae2828d5b011d6c4d3dc93c3105
gift56 <efeasiughu@gmail.com>
2022-03-15 09:52:16 +0100

    practicing js

---
+++ styles/js/function.js
@@ -0,0 +1,22 @@
+// alert("Welcome to Atc-Hub: Question");
+/*let userName = prompt("Enter your name: ");
+uname.innerHTML = userName;
+
+let Useradd = prompt("Enter your address: ");
+add.innerHTML = Useradd;
+
+let Userage = prompt("Enter your age: ");
+age.innerHTML = Userage;*/
+
+//added onclick function on an Id dat to display date
+function displayDate() {
+    dat.innerHTML = Date();
+}
+
+let task = 1 + "week", Task = 6024800 + "s";
+
+function myBtn() {
+    document.getElementById("minute").innerHTML = task;
+    document.getElementById("second").innerHTML = Task;
+}
+// alert("Looking good");
